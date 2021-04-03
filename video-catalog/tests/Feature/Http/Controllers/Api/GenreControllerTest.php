701656562b97221bad205cfd6342b210094ea69a
Gabriel <gabriel.stamato@certsys.com.br>
2021-04-03 16:17:46 -0300

    Feat: Add Http Delete tests

--- video-catalog/tests/Feature/Http/Controllers/Api/GenreControllerTest.php
+++ video-catalog/tests/Feature/Http/Controllers/Api/GenreControllerTest.php
@@ -129,4 +129,15 @@ class GenreControllerTest extends TestCase
                 'is_active' => true
             ]);
     }
+
+    public function testDestroy()
+    {
+        $genre = factory(Genre::class)->create();
+        $response = $this->json('DELETE', route('genres.destroy', ['genre' => $genre->id]));
+        
+        $response->assertStatus(204);
+
+        $this->assertNull(Genre::find($genre->id));
+        $this->assertNotNull(Genre::withTrashed()->find($genre->id));
+    }
 }
